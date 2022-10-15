0e46fe8b5fef9d20ecce2058ac9a04559ed09324
Aravind M <109063587+Aravindx@users.noreply.github.com>
2022-10-15 11:31:46 +0530

    Create PersonClass.js

---
+++ PersonClass.js
@@ -0,0 +1,14 @@
+class person{
+    constructor(Name,DOB,age,gender,mobileNum,Qualification,Experience){
+        this.Name=Name;
+        this.DOB=DOB;
+        this.age=age;
+        this.gender=gender;
+        this.mobileNum=mobileNum;
+        this.Qualification=Qualification;
+        this.Experience=Experience;
+    }
+
+}
+var details=new person("Aravind M","19/06/2001",21,"Male",898989898989,"BE BIOMEDICAL","Fresher")
+console.log(details)
